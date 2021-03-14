429f2c09dcf11bc3a8e05ff6cc8c8afb9862dd39
calebddawson <calebdawsondev@gmail.com>
2021-03-14 12:34:12 -0500

    Make Menu bar dynamic

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 session_start();
 include ("connect.php");
 if (!isset($_SESSION["user_login"])) {
+$username = "";
 }
 else {
 $username = $_SESSION["user_login"];
@@ -15,7 +16,7 @@ $username = $_SESSION["user_login"];
     <link rel="stylesheet" type="text/css" href="style.css"/>
   </head>
   <body>
-    <div class="header">
+    <!--<div class="header">
       <div class="headerWrapper">
         <div class="headerLogo">
           Group-9
@@ -30,6 +31,46 @@ $username = $_SESSION["user_login"];
           <a href="#" >About</a>
           <a href="#" >Create Account</a>
           <a href="#" >Sign In</a>
+        </div>
+      </div>
+    </div>
+-->
+
+    <div class="header">
+        <div class="headerWrapper">
+          <div class="headerObject">
+            <a href="#" class="headerLink" >Group 9 </a>
+          </div>
+
+          <?php
+          if (isset($_SESSION["user_login"])) {
+          echo '
+          <div class="headerObject">
+            <a href="'.$username.'" class="headerLink" >Profile</a>
+          </div>
+          <div class="headerObject">
+            <a href="logout.php" class="headerLink" >Logout</a>
+          </div>
+          ';
+          }
+          else
+          {
+            echo '
+            <div class="headerObject">
+              <a href="index.php" class="headerLink" >Create Account </a>
+            </div>
+            <div class="headerObject">
+              <a href="index.php" class="headerLink" >Login </a>
+            </div>
+            ';
+          }
+          ?>
+
+          <div class="headerSearch">
+            <form action="search.php" method="GET" id="search">
+              <input type="text" name="q" size="60" placeholder="Search ..."/>
+            </form>
+          </div>
         </div>
       </div>
     </div>
