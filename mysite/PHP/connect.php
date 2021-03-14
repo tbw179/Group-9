2c53819180d51ea4f93713b15a3a9c74c1c3b177
calebddawson <calebdawsondev@gmail.com>
2021-03-14 01:21:02 -0600

    Connect to Database

--- mysite/PHP/connect.php
+++ mysite/PHP/connect.php
@@ -1,1 +1,12 @@
+<?php
+$host="localhost";
+$username="id16376105_admin";
+$password='$SQ6[K-R5)}Qu-nY';
+$dbname="id16376105_users";
 
+$conn=mysqli_connect($host, $username, $password, $dbname);
+if(mysqli_connect_errno())
+    echo "Connection could not be established...".mysqli_connect_error();
+else
+    echo "Successfully connected...";
+?>
