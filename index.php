2b1a0e869d3420fb42c808a726b668e80fe72fad
calebddawson <calebdawsondev@gmail.com>
2021-03-14 05:58:00 -0500

    Moved Registration form to separate file

--- index.php
+++ index.php
@@ -26,47 +26,6 @@
 
 <?php include ("./mysite/PHP/connect.php"); ?>
 <?php include ("./mysite/PHP/register.php"); ?>
-<!--$reg = @$_POST['reg'];
-$fn = ""; // First Name
-$ln = ""; // Last Name
-$un = ""; // Username
-$pswd = ""; // Password
-
-$u_check = ""; // Check if username exists
-// Registration form
-$fn = strip_tags(@$_POST['fname']);
-$ln = strip_tags(@$_POST['lname']);
-$un = strip_tags(@$_POST['username']);
-$pswd = strip_tags(@$_POST['password']);
-
-if ($reg) {
-$u_check = mysqli_query($conn, "SELECT username FROM users WHERE username='$un'");
-$check = mysqli_num_rows($u_check);
-if ($check == 0) {
-if ($fn&&$ln&&$un&&$pswd) {
-if (strlen($un)>25||strlen($fn)>25||strlen($ln)>25) {
-echo "The maximum limit for username/first name/last name is 25 characters.";
-}
-else {
-if (strlen($pswd)>30||strlen($pswd)<5) {
-echo "Your password must be between 5 and 30 characters long.";
-}
-else {
-$pswd = md5($pswd);
-$query = mysqli_query($conn, "INSERT INTO users VALUES ('$un', '$fn', '$ln', '$pswd')");
-die("<h2>Welcome to Group-9</h2>Login to your account to get started.");
-}
-}
-}
-else {
-echo "Please fill in all of the fields.";
-}
-}
-else {
-echo "Username already taken.";
-}
-}
-?>-->
 
     <div class="tableWrapper">
       <table>
