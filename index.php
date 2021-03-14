6637ce301931288993cac999548e152edbd32584
calebddawson <calebdawsondev@gmail.com>
2021-03-14 05:20:36 -0500

    Add Registration Form

--- index.php
+++ index.php
@@ -23,6 +23,50 @@
         </div>
       </div>
     </div>
+
+<?php
+$reg = @$_POST['reg'];
+$fn = ""; // First Name
+$ln = ""; // Last Name
+$un = ""; // Username
+$pswd = ""; // Password
+
+$u_check = ""; // Check if username exists
+// Registration form
+$fn = strip_tags(@$_POST['fname']);
+$ln = strip_tags(@$_POST['lname']);
+$un = strip_tags(@$_POST['username']);
+$pswd = strip_tags(@$_POST['password']);
+
+if ($reg) {
+$u_check = mysql_query("SELECT username FROM users WHERE username='$un'");
+$check = mysql_num_rows($u_check);
+if ($check == 0) {
+if ($fn&&$ln&&$un&&$pswd) {
+if (strlen($un)>25||strlen($fn)>25||strlen($ln)>25) {
+echo "The maximum limit for username/first name/last name is 25 characters.";
+}
+else {
+if (strlen($pswd)>30||strlen($pswd)<5) {
+echo "Your password must be between 5 and 30 characters long.";
+}
+else {
+$pswd = md5($pswd);
+$query = mysql_query("INSERT INTO users VALUES ('', '$un', '$fn', '$ln', '$pswd')");
+die("<h2>Welcome to Group-9</h2>Login to your account to get started.");
+}
+}
+}
+else {
+echo "Please fill in all of the fields.";
+}
+}
+else {
+echo "Username already taken.";
+}
+}
+?>
+
     <div class="tableWrapper">
       <table>
         <tr>
@@ -36,7 +80,7 @@
               <input type="text" name="lname" size="25" placeholder="Last Name"/><br><br>
               <input type="text" name="username" size="25" placeholder="Email"/><br><br>
               <input type="text" name="password" size="25" placeholder="Password"/><br><br>
-              <input type="submit" name="submit" value="Create Account"/>
+              <input type="submit" name="reg" value="Create Account"/>
 
             </form>
           </td>
